d7c5d10114d6d186acc244be898e2eed6b6f79a6
tom_m <tomcafe0161@gmail.com>
2026-02-06 09:26:32 +0900

    マージ

--- Assets/Scripts/SystemManager.cs
+++ Assets/Scripts/SystemManager.cs
@@ -7,34 +7,31 @@ using UnityEditorInternal;   // ReorderableList
 // ================= 通常名前空間 =================
 using System.Collections;
 using System.Collections.Generic;
-<<<<<<< HEAD
-//using Unity.VisualScripting.ReorderableList.Internal;
-=======
->>>>>>> origin/washida2
 using UnityEngine;
-
 
 public class SystemManager : MonoBehaviour
 {
-	public static SystemManager Instance { get; private set; }
+    public static SystemManager Instance { get; private set; }
 
-	public CardEffectHelper cardEffectHelper { get; private set; }
-	public CardMotionHelper cardMotionHelper { get; private set; }
-	public UIHelper uiHelper { get; private set; }
+    public CardEffectHelper cardEffectHelper { get; private set; }
+    public CardMotionHelper cardMotionHelper { get; private set; }
+    public UIHelper uiHelper { get; private set; }
 
-	private void Awake()
-	{
-		if (Instance == null)
-		{
-			Instance = this;
-			DontDestroyOnLoad(gameObject);
-			if (cardEffectHelper == null) cardEffectHelper = GetComponent<CardEffectHelper>();
-			if (cardMotionHelper == null) cardMotionHelper = GetComponent<CardMotionHelper>();
-			if (uiHelper == null) uiHelper = GetComponent<UIHelper>();
-		}
-		else
-		{
-			Destroy(gameObject);
-		}
-	}
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            // 各ヘルパーコンポーネントの取得
+            if (cardEffectHelper == null) cardEffectHelper = GetComponent<CardEffectHelper>();
+            if (cardMotionHelper == null) cardMotionHelper = GetComponent<CardMotionHelper>();
+            if (uiHelper == null) uiHelper = GetComponent<UIHelper>();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
